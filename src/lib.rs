9bcdaa759aa825c7aacf295e831df7e0ed52fb8b
AljoschaMeyer <mail@aljoscha-meyer.de>
2018-03-16 16:05:36 +0100

    Use try_ready

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 //! Utilities for working with `std::io` and `futures_io`.
 #![deny(missing_docs)]
 
+#[macro_use(try_ready)]
 extern crate futures_core;
 extern crate futures_io;
 #[cfg(feature = "quickcheck")]
