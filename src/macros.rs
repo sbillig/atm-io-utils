c29fdf8e6935db87376d2471d5603b6c542fc096
AljoschaMeyer <mail@aljoscha-meyer.de>
2018-03-16 14:14:00 +0100

    Add read_nz and write_nz macros

--- src/macros.rs
+++ src/macros.rs
@@ -14,3 +14,27 @@ macro_rules! retry {
         }
     )
 }
+
+/// A variant of try_ready! that checks whether the expression evaluates to 0, and emits a
+/// `futures_io::Error` of kind `UnexpectedEof` with the given message if so.
+#[macro_export]
+macro_rules! read_nz {
+    ($e:expr, $msg:expr) => (
+        match try_ready!($e) {
+            0 => return Err(::futures_io::Error::new(::futures_io::ErrorKind::UnexpectedEof, $msg).into()),
+            read => read
+        }
+    )
+}
+
+/// A variant of try_ready! that checks whether the expression evaluates to 0, and emits a
+/// `futures_io::Error` of kind `WriteZero` with the given message if so.
+#[macro_export]
+macro_rules! write_nz {
+    ($e:expr, $msg:expr) => (
+        match try_ready!($e) {
+            0 => return Err(::futures_io::Error::new(::futures_io::ErrorKind::WriteZero, $msg).into()),
+            written => written
+        }
+    )
+}
